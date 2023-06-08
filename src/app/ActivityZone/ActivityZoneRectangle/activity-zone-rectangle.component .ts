71cf4de2a98ed225a511d8316fc7526300772fbb
MilosLukicBM <milos.lukic@miracl.at>
2023-06-08 18:42:01 +0200

    added deletion functionality

--- src/app/ActivityZone/ActivityZoneRectangle/activity-zone-rectangle.component .ts
+++ src/app/ActivityZone/ActivityZoneRectangle/activity-zone-rectangle.component .ts
@@ -1,13 +1,9 @@
-import {
-  Component,
-  Input,
-  OnDestroy,
-  OnInit,
-  TemplateRef,
-  ViewChild,
-} from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ActivityZone } from 'src/types/ActivityZone';
 import { fabric } from 'fabric';
+import { AppState } from 'src/state/app.state';
+import { Store } from '@ngrx/store';
+import { removeActivityZone } from 'src/state/activityZones/activityZone.actions';
 
 @Component({
   selector: 'app-activity-zone-rectangle',
@@ -29,8 +25,37 @@ export class ActivityZoneRectangleComponent implements OnInit, OnDestroy {
       height,
       name: id,
       lockRotation: true,
-      opacity: 0.5
+      opacity: 0.5,
+      hasControls: true,
     });
+
+    const img = document.createElement('img');
+    img.src = '../../../assets/delete.png';
+
+    (fabric.Object.prototype.controls as any).deleteControl =
+      new fabric.Control({
+        x: 0.5,
+        y: -0.5,
+        offsetY: -16,
+        offsetX: 16,
+        cursorStyle: 'pointer',
+        mouseUpHandler: (_, transform) => {
+          this.store.dispatch(removeActivityZone({ id: transform.target.name || "" }));
+          this.parentCanvas.requestRenderAll();
+          return true;
+        },
+        render: (ctx, left, top, styleOverride, fabricObject) => {
+          const size = (this.zone.cornerSize || 10) * 2;
+          ctx.save();
+          ctx.translate(left, top);
+          ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle || 0));
+          ctx.drawImage(img, -size / 2, -size / 2, size, size);
+          ctx.restore();
+          styleOverride = {
+            borderRadius: '50%',
+          };
+        },
+      });
 
     this.parentCanvas.add(this.zone);
     this.parentCanvas.setActiveObject(this.zone);
@@ -39,4 +64,6 @@ export class ActivityZoneRectangleComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.parentCanvas.remove(this.zone);
   }
+
+  constructor(private store: Store<AppState>) {}
 }
