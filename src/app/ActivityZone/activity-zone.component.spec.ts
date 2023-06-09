389c543b27e475cb1c1ef04ebe1d34efb73865df
MilosLukicBM <milos.lukic@miracl.at>
2023-06-09 20:05:29 +0200

    attempt to import store in test

--- src/app/ActivityZone/activity-zone.component.spec.ts
+++ src/app/ActivityZone/activity-zone.component.spec.ts
@@ -1,6 +1,41 @@
+import { ActivityZone } from './../../types/ActivityZone/index';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ActivityZoneComponent } from './activity-zone.component';
+import {
+  DEFAULT_COLOR,
+  DEFAULT_HEIGHT,
+  DEFAULT_WIDTH,
+  DEFAULT_X,
+  DEFAULT_Y,
+} from 'src/constants/activityZone';
+import { StoreModule, createReducer, on } from '@ngrx/store';
+import { loadActivityZones } from 'src/state/activityZones/activityZone.actions';
+import { ActivityZonesState } from 'src/state/activityZones/activityZone.reducers';
+
+export const initialState: ActivityZonesState = {
+  activityZones: [],
+  xScale: 1,
+  yScale: 1,
+};
+
+const mockZone: ActivityZone = {
+  id: String(Date.now()),
+  color: DEFAULT_COLOR,
+  height: DEFAULT_HEIGHT,
+  width: DEFAULT_WIDTH,
+  x: DEFAULT_X,
+  y: DEFAULT_Y,
+};
+
+export const mockActivityZoneReducer = createReducer(
+  initialState,
+  on(loadActivityZones, () => {
+    return {
+      ...initialState,
+      activityZones: [mockZone],
+    };
+  })
+);
 
 describe('ActivityZoneComponent', () => {
   let component: ActivityZoneComponent;
@@ -8,7 +43,8 @@ describe('ActivityZoneComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ActivityZoneComponent]
+      declarations: [ActivityZoneComponent],
+      imports: [StoreModule.forRoot({activityZones: mockActivityZoneReducer}, {})],
     });
     fixture = TestBed.createComponent(ActivityZoneComponent);
     component = fixture.componentInstance;
@@ -17,5 +53,20 @@ describe('ActivityZoneComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('load activity zones', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should have loaded the proper zones', () => {
+      component.ngOnInit();
+
+      component.allActivityZones$.subscribe((zones) => {
+        expect(zones.length).toBe(1);
+        expect(zones[0]).toBe(mockZone);
+      });
+    });
   });
 });
